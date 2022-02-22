d0b7e8039cd5b1c25afe4c017222404ffc12b826
angularsantanderii <angularsantanderii@gmail.com>
2022-02-22 08:15:09 +0000

    acceso dom

--- src/app/acceso-dom/acceso-dom.component.ts
+++ src/app/acceso-dom/acceso-dom.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-acceso-dom',
@@ -8,6 +8,7 @@ import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/co
 export class AccesoDOMComponent implements OnInit {
 
   @ViewChild('lista') listaRef: ElementRef; // Sustituye a document.getElementByid('lista')
+  @ViewChildren('item') itemsRef: QueryList<any>;
 
   constructor(private renderer: Renderer2) { }
 
@@ -17,6 +18,18 @@ export class AccesoDOMComponent implements OnInit {
 
   ngAfterViewInit(): void {
     this.renderer.setStyle(this.listaRef.nativeElement, 'padding-left', '200px'); // style...
+  }
+
+  setDone(i: number): void {
+    this.itemsRef.forEach((elem: any, index: number) => {
+      if(index === i) {
+        if(elem.nativeElement.style.textDecoration === '') {
+          this.renderer.setStyle(elem.nativeElement, 'text-decoration', 'line-through');
+        } else {
+          this.renderer.setStyle(elem.nativeElement, 'text-decoration', '');
+        }
+      }
+    })
   }
 
 }
